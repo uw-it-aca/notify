15b8c37b70e50a4d6d3a41dbb2d2fa73a9bda319
Jim Laney <jlaney@uw.edu>
2017-07-13 09:57:34 -0700

    adds registration_consumer command

--- notify/cache_implementation.py
+++ notify/cache_implementation.py
@@ -5,6 +5,9 @@ import re
 class UICache(TimedCache):
     """ A custom cache implementation for CAN """
 
+    kws_url_current_key = '/key/v1/type/%s/encryption/current'
+    kws_url_key = '/key/v1/encryption/%s.json'
+
     url_policies = {}
     url_policies["sws"] = (
         (re.compile(r"^/student/v5/term/"), 60 * 60 * 10),
@@ -15,6 +18,25 @@ class UICache(TimedCache):
     url_policies["nws"] = (
         (re.compile(r"^/notification/v1/channel"), 60 * 60 * 10),
     )
+    url_policies["kws"] = (
+        (re.compile(r"^%s" % (
+            kws_url_key % '[\-\da-fA-F]{36}\\')), 60 * 60 * 24 * 30),
+        (re.compile(r"^%s" % (
+            kws_url_current_key % "[\-\da-zA-Z]+")), 60 * 60 * 24 * 7),
+    )
+
+    def deleteCache(self, service, url):
+        try:
+            entry = CacheEntryTimed.objects.get(service=service, url=url)
+            entry.delete()
+        except CacheEntryTimed.DoesNotExist:
+            return
+
+    def delete_cached_kws_current_key(self, resource_type):
+        self.deleteCache('kws', self.kws_url_current_key % resource_type)
+
+    def delete_cached_kws_key(self, key_id):
+        self.deleteCache('kws', self.kws_url_key % key_id)
 
     def getCache(self, service, url, headers):
         cache_time = 0
