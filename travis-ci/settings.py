096e4316d3c7cd0cb66f41bbee8cbdca6996fe0b
Jim Laney <jlaney@uw.edu>
2019-12-02 16:04:49 -0800

    update travis settings, fix urls.py

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -1,86 +1,53 @@
-"""
-Django settings for project project.
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'debug': DEBUG,
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-ALLOWED_HOSTS = []
-
-STATIC_ROOT = ''
-
-# Application definition
-
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'compressor',
-    'templatetag_handlebars',
+    'django_user_agents',
     'supporttools',
     'userservice',
-    'notify.apps.NotifyUIConfig',
+    'persistent_message',
     'rc_django',
+    'notify.apps.NotifyUIConfig',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.RemoteUserMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+    'django.contrib.auth.middleware.PersistentRemoteUserMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
+]
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.RemoteUserBackend',
-#    'django.contrib.auth.backends.ModelBackend',
 )
 
 ROOT_URLCONF = 'travis-ci.urls'
 
 WSGI_APPLICATION = 'travis-ci.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
 
 DATABASES = {
     'default': {
@@ -88,9 +55,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -102,19 +66,37 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {},
-    'formatters': {},
-    'handlers': {},
-    'loggers': {},
+    'filters': {
+        'add_user': {
+            '()': 'notify.log.UserFilter'
+        },
+    },
+    'formatters': {
+        'notify': {
+            'format': '%(asctime)s %(user)s %(actas)s %(message)s',
+            'datefmt': '[%Y-%m-%d %H:%M:%S]',
+        },
+    },
+    'handlers': {
+        'notify': {
+            'class': 'logging.StreamHandler',
+            'filters': ['add_user'],
+            'formatter': 'notify',
+        },
+    },
+    'loggers': {
+        'notify': {
+            'handlers': ['notify'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
 }
 
 NOTIFY_ADMIN_GROUP = 'test'
