50458218c2d8be435671eaa12ed83437580b1511
Jim Laney <jlaney@uw.edu>
2020-07-01 10:11:03 -0700

    moves test to external secrets

--- docker/settings.py
+++ docker/settings.py
@@ -48,6 +48,107 @@ COMPRESS_OFFLINE_CONTEXT = {
     'wrapper_template': 'persistent_message/manage_wrapper.html',
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'add_user': {
+            '()': 'notify.log.UserFilter'
+        },
+        'stdout_stream': {
+            '()': 'django.utils.log.CallbackFilter',
+            'callback': lambda record: record.levelno <= logging.WARNING
+        },
+        'stderr_stream': {
+            '()': 'django.utils.log.CallbackFilter',
+            'callback': lambda record: record.levelno >= logging.ERROR
+        }
+    },
+    'formatters': {
+        'ui': {
+            'format': '%(levelname)-4s %(asctime)s %(user)s %(actas)s %(message)s [%(name)s]',
+            'datefmt': '[%Y-%m-%d %H:%M:%S]',
+        },
+        'restclients_timing': {
+            'format': '%(levelname)-4s restclients_timing %(module)s %(asctime)s %(message)s [%(name)s]',
+            'datefmt': '[%Y-%m-%d %H:%M:%S]',
+        },
+        'events': {
+            'format': '%(levelname)-4s event %(asctime)s %(message)s [%(name)s]',
+            'datefmt': '[%Y-%m-%d %H:%M:%S]',
+        },
+    },
+    'handlers': {
+        'stdout': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'filters': ['add_user', 'stdout_stream'],
+            'formatter': 'ui',
+        },
+        'stderr': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+            'filters': ['add_user', 'stderr_stream'],
+            'formatter': 'ui',
+        },
+        'event_log': {
+            'filters': ['stdout_stream'],
+            'formatter': 'events',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        },
+        'ui_log': {
+            'filters': ['add_user', 'stdout_stream'],
+            'formatter': 'ui',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        },
+        'restclients_timing': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'filters': ['stdout_stream'],
+            'formatter': 'restclients_timing',
+        },
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django.security.DisallowedHost': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['stderr'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'notify': {
+            'handlers': ['ui_log'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'notify.events': {
+            'handlers': ['event_log'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'restclients_core': {
+            'handlers': ['restclients_timing'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['stdout', 'stderr'],
+            'level': 'INFO' if os.getenv('ENV', 'localdev') == 'prod' else 'DEBUG'
+        }
+    }
+}
+
 if os.getenv('ENV') == 'localdev':
     DEBUG = True
     NOTIFY_ADMIN_GROUP = 'u_test_group'
@@ -55,8 +156,6 @@ if os.getenv('ENV') == 'localdev':
 else:
     NOTIFY_ADMIN_GROUP = os.getenv('ADMIN_GROUP', '')
     RESTCLIENTS_DAO_CACHE_CLASS = 'notify.cache_implementation.NotifyMemcachedCache'
-    if os.getenv('ENV') == 'prod':
-        APP_SERVER_BASE = 'https://notify.uw.edu'
 
 USERSERVICE_VALIDATION_MODULE = 'notify.utilities.validate_override_user'
 USERSERVICE_OVERRIDE_AUTH_MODULE = 'notify.views.can_override_user'
@@ -95,8 +194,8 @@ AWS_SQS = {
 GOOGLE_ANALYTICS_KEY = os.getenv('GOOGLE_ANALYTICS_KEY', '')
 SUPPORTTOOLS_PARENT_APP = 'Notify.UW'
 SUPPORTTOOLS_PARENT_APP_URL = '/'
-SUPPORT_EMAIL = 'help@uw.edu'
-SENDER_ADDRESS = 'notify@uw.edu'
+SUPPORT_EMAIL = os.getenv('SUPPORT_EMAIL_ADDRESS', '')
+SENDER_ADDRESS = os.getenv('EMAIL_SENDER_ADDRESS', '')
 
 INVALID_UUIDS = [
     '00000000-0000-0000-0000-000000000000'
